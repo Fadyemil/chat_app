398569df08daf0e9a27545ff1866955e01af5ac8
fady emil <fady46t45f@gmail.com>
2024-08-14 19:17:47 +0300

    chat screen UI

--- lib/features/chat/screen/chat_screen.dart
+++ lib/features/chat/screen/chat_screen.dart
@@ -1,13 +1,43 @@
+import 'package:chat_app/features/chat/widget/add_friend.dart';
+import 'package:chat_app/features/chat/widget/list_chat.dart';
+// import 'package:chat_app/features/chat/widget/chat_card.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  TextEditingController emailCon = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Text('ChatScreen'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddFriend(emailCon: emailCon);
+            },
+          );
+        },
+        child: const Icon(Iconsax.message_add),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListChat(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
