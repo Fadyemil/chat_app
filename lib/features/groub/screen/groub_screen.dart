57f37c8a5b11667a9c6771032d6591f2d63faad6
fady emil <fady46t45f@gmail.com>
2024-08-14 16:19:00 +0300

    add baseic class for app

---
+++ lib/features/groub/screen/groub_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroubScreen extends StatelessWidget {
+  const GroubScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('groub-screen'),
+    );
+  }
+}
