1eb01f21369824f1e53373055ea45099bdefe144
fady emil <fady46t45f@gmail.com>
2024-08-14 19:19:38 +0300

    add all widget for chat screen

---
+++ lib/features/chat/screen/details_chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:chat_app/features/chat/widget/app_bar_details_chat_screen.dart';
+import 'package:chat_app/features/chat/widget/list_message_card.dart';
+import 'package:chat_app/features/chat/widget/send_message_field.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class DetailsChatScreen extends StatelessWidget {
+  const DetailsChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarDetailsChatScreen(context),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListMessageCard(),
+              // child: Center(
+              //   child: GestureDetector(
+              //     child: WelcomeMessage(),
+              //   ),
+              // ),
+            ),
+            Row(
+              children: [
+                const Expanded(
+                  child: SendMessageField(),
+                ),
+                IconButton.filled(
+                  onPressed: () {},
+                  icon: const Icon(Iconsax.send_1),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
