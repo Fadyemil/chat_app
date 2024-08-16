f279da274af030fbca28e8b56ba087c327d39ffa
fady emil <fady46t45f@gmail.com>
2024-08-16 17:12:35 +0300

    start firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 // import 'package:chat_app/features/auth/ui/screen/login_screen.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/layout.dart';
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) {
-        return const MyApp();
-      },
-    ),
+    // DevicePreview(
+    //   enabled: true,
+    //   builder: (context) {
+         const MyApp()
+    //   },
+    // ),
   );
 }
 
