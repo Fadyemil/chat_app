422f3130227b1c151e38c2003220743b7847af22
fady emil <fady46t45f@gmail.com>
2024-08-22 17:44:03 +0300

    send message

--- lib/features/groub/screen/details_groub_screen.dart
+++ lib/features/groub/screen/details_groub_screen.dart
@@ -1,43 +1,56 @@
-import 'package:chat_app/features/groub/widget/group_message_card.dart';
-// import 'package:chat_app/features/chat/widget/welcome_message.dart';
+import 'package:chat_app/core/models/groub_model.dart';
+import 'package:chat_app/features/chat/data/firebase/fire_data_base.dart';
 import 'package:chat_app/features/groub/widget/widget_details_groub/app_bar_details_screen.dart';
+import 'package:chat_app/features/groub/widget/widget_details_groub/list_chat_messages.dart';
 import 'package:chat_app/features/groub/widget/widget_details_groub/send_message_groub.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
-class DetailsGroubScreen extends StatelessWidget {
-  const DetailsGroubScreen({super.key});
+class DetailsGroubScreen extends StatefulWidget {
+  const DetailsGroubScreen({super.key, required this.groubModel});
+  final GroubModel groubModel;
+
+  @override
+  State<DetailsGroubScreen> createState() => _DetailsGroubScreenState();
+}
+
+class _DetailsGroubScreenState extends State<DetailsGroubScreen> {
+  TextEditingController sendMessageGroub = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarDetailsFroubScreen(context),
+      appBar: AppBarDetailsFroubScreen(context, widget.groubModel),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
         child: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                reverse: true,
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  return GroupMessageCard(
-                    index: index,
-                  );
-                },
-              ),
-              // child: Center(
-              //   child: GestureDetector(
-              //     child: WelcomeMessage(),
-              //   ),
-              // ),
+              child: ListChatMessages(groubModel: widget.groubModel,),
+             
             ),
             Row(
               children: [
                 Expanded(
-                  child: SendMessageGroub(),
+                  child: SendMessageGroub(
+                    sendMessageGroub: sendMessageGroub,
+                  ),
                 ),
-                IconButton.filled(onPressed: () {}, icon: Icon(Iconsax.send_1))
+                IconButton.filled(
+                    onPressed: () {
+                      if (sendMessageGroub.text.isNotEmpty) {
+                        FireDataBase()
+                            .sendGroubMessage(
+                                msg: sendMessageGroub.text,
+                                groupId: widget.groubModel.id!)
+                            .then((v) => {
+                                  setState(() {
+                                    sendMessageGroub.text = "";
+                                  })
+                                });
+                      }
+                    },
+                    icon: Icon(Iconsax.send_1))
               ],
             )
           ],
