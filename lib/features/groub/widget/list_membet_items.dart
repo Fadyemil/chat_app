756e4a588cf4bfb87d5bc1c8b419a8d29173b720
fady emil <fady46t45f@gmail.com>
2024-08-22 18:03:21 +0300

    add member show in Ui

--- lib/features/groub/widget/list_membet_items.dart
+++ lib/features/groub/widget/list_membet_items.dart
@@ -1,5 +1,7 @@
+import 'package:chat_app/core/models/chat_user_model.dart';
 import 'package:chat_app/core/models/groub_model.dart';
 import 'package:chat_app/features/groub/widget/member_items.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ListMembetItems extends StatelessWidget {
@@ -12,16 +14,27 @@ class ListMembetItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: null,
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('id', whereIn: groubModel.members)
+            .snapshots(),
         builder: (context, snapshot) {
-          return ListView.builder(
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return MemberItems(
-                groubModel: groubModel,
-              );
-            },
-          );
+          if (snapshot.hasData) {
+            List<ChatUserModel> members = snapshot.data!.docs
+                .map((e) => ChatUserModel.fromJson(e.data()))
+                .toList();
+            return ListView.builder(
+              itemCount: members.length,
+              itemBuilder: (context, index) {
+                return MemberItems(
+                  groubModel: groubModel,
+                  members: members[index],
+                );
+              },
+            );
+          } else {
+            return Container();
+          }
         });
   }
 }
--- lib/features/groub/widget/member_items.dart
+++ lib/features/groub/widget/member_items.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/models/chat_user_model.dart';
 import 'package:chat_app/core/models/groub_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +8,23 @@ class MemberItems extends StatelessWidget {
   const MemberItems({
     super.key,
     required this.groubModel,
+    required this.members,
   });
   final GroubModel groubModel;
-
+  final ChatUserModel members;
   @override
   Widget build(BuildContext context) {
     bool isAdmin =
         groubModel.admin!.contains(FirebaseAuth.instance.currentUser!.uid);
+    bool admin = groubModel.admin!.contains(members.id);
     return ListTile(
-      title: Text("Name"),
-      subtitle: Text("Admin"),
+      title: Text(members.name!),
+      subtitle: admin
+          ? Text(
+              "Admin",
+              style: TextStyle(color: Colors.green),
+            )
+          : Text('member'),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
