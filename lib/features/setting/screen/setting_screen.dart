0ee7f65536663ecdfcd1af8c790968a46b499793
fady emil <fady46t45f@gmail.com>
2024-08-24 17:19:39 +0300

    edit ui in setting screen

--- lib/features/setting/screen/setting_screen.dart
+++ lib/features/setting/screen/setting_screen.dart
@@ -1,9 +1,12 @@
+// import 'package:chat_app/core/models/cloud_messaging.dart';
+// import 'package:chat_app/features/setting/logic/get_user_cubit/get_user_cubit.dart';
 import 'package:chat_app/features/setting/widget/change_mode.dart';
 import 'package:chat_app/features/setting/widget/change_theme.dart';
 import 'package:chat_app/features/setting/widget/profile_item.dart';
 import 'package:chat_app/features/setting/widget/signout_item.dart';
 import 'package:chat_app/features/setting/widget/user_data.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -11,6 +14,17 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // PushNotificationService.sendNotificationToAdmin(
+          //   deviceToken: context.read<GetUserCubit>().me!.puchToken!,
+          //   context: context,
+          //   // tripId,
+          //   messageBody: 'Hello world',
+          // );
+        },
+        child: Icon(Icons.send_sharp),
+      ),
       appBar: AppBar(title: const Text("Settings")),
       body: const Padding(
         padding: EdgeInsets.all(20.0),
@@ -31,4 +45,3 @@ class SettingScreen extends StatelessWidget {
     );
   }
 }
-
